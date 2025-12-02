7a3dcb0a7b31d9c70b25e7c0f393cb288c53f51f
Samuel Onyedikachi <samuelonyedikachi450@gmail.com>
2025-12-02 22:22:13 +0100

    update trends

--- src/Components/lowlevel/Card.jsx
+++ src/Components/lowlevel/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
   return (
-    <div className="rounded-2xl p-6 bg-[#262626] hover:bg-[#333333] transition-all duration-300 transform hover:-translate-y-2 shadow-lg">
+    <div className="rounded-xl p-6 bg-[#262626] hover:bg-[#313030] transition-all duration-300 transform hover:-translate-y-2 shadow-lg">
       {children}
     </div>
   );
--- src/Components/lowlevel/GridCards.jsx
+++ src/Components/lowlevel/GridCards.jsx
@@ -1,7 +1,7 @@
 const GridCards = ({ children, lgCol }) => {
   return (
     <div
-      className={`grid grid-cols-1 md:grid-cols-2 xl:grid-cols-${lgCol} gap-6 my-8`}
+      className={`grid grid-cols-1 md:grid-cols-3 xl:grid-cols-${lgCol} gap-8 my-8`}
     >
       {children}
     </div>
